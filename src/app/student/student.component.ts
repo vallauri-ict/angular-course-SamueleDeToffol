74f1364095b26f0b8b2d271e84cd1a4abe0d6a34
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-10-07 11:27:15 +0200

    Lesson 1 - Step 5 : click handler for student component and global clickable class

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -8,27 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent implements OnInit {
 
   studentList = [
-    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M'},
-    { name: 'Giada Valinotto', hobby: 'Paddle', gender: 'F'},
-    { name: 'Luca Dellavalle', hobby: 'Poligono', gender: 'M'},
-    { name: 'Bianca Teleman', hobby: 'Karate', gender: 'F'},
-    { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M'},
-    { name: 'Samuele De Toffol', hobby: 'Paddle', gender: 'M'},
-    { name: 'Luna Martino', hobby: 'Poligono', gender: 'F'},
-    { name: 'Matteo Cela', hobby: 'Karate', gender: 'M'},
-    { name: 'Gabriele Leone', hobby: 'Palestra', gender: 'M'},
-    { name: 'Martina Velardi', hobby: 'Karate', gender: 'F'},
-    { name: 'Marta Baudracco', hobby: 'Paddle', gender: 'F'},
-    { name: 'Paolo Giordano', hobby: 'Palestra', gender: 'M'},
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
+    { name: 'Giada Valinotto', hobby: 'Paddle', gender: 'F' },
+    { name: 'Luca Dellavalle', hobby: 'Poligono', gender: 'M' },
+    { name: 'Bianca Teleman', hobby: 'Karate', gender: 'F' },
+    { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M' },
+    { name: 'Samuele De Toffol', hobby: 'Paddle', gender: 'M' },
+    { name: 'Luna Martino', hobby: 'Poligono', gender: 'F' },
+    { name: 'Matteo Cela', hobby: 'Karate', gender: 'M' },
+    { name: 'Gabriele Leone', hobby: 'Palestra', gender: 'M' },
+    { name: 'Martina Velardi', hobby: 'Karate', gender: 'F' },
+    { name: 'Marta Baudracco', hobby: 'Paddle', gender: 'F' },
+    { name: 'Paolo Giordano', hobby: 'Palestra', gender: 'M' },
   ]
 
-  student : any = {};
+  student: any = {};
   constructor() {
-    let num = Math.floor(Math.random()*this.studentList.length);
-    this.student = this.studentList[num];
-   }
+    this.randomStudent();
+  }
 
+  private randomStudent() {
+    let num = Math.floor(Math.random() * this.studentList.length);
+    this.student = this.studentList[num];
+  }
   ngOnInit(): void {
   }
 
+  onStudentClick() {
+    this.randomStudent();
+  }
 }
