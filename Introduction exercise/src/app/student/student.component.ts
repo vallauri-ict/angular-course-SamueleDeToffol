c4685eff8c44f26dad61d00e07fbd676657413df
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-10-14 10:57:25 +0200

    ngIf directive

--- Introduction exercise/src/app/student/student.component.ts
+++ Introduction exercise/src/app/student/student.component.ts
@@ -8,18 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent implements OnInit {
 
   studentList = [
-    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
-    { name: 'Giada Valinotto', hobby: 'Paddle', gender: 'F' },
-    { name: 'Luca Dellavalle', hobby: 'Poligono', gender: 'M' },
-    { name: 'Bianca Teleman', hobby: 'Karate', gender: 'F' },
-    { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M' },
-    { name: 'Samuele De Toffol', hobby: 'Paddle', gender: 'M' },
-    { name: 'Luna Martino', hobby: 'Poligono', gender: 'F' },
-    { name: 'Matteo Cela', hobby: 'Karate', gender: 'M' },
-    { name: 'Gabriele Leone', hobby: 'Palestra', gender: 'M' },
-    { name: 'Martina Velardi', hobby: 'Karate', gender: 'F' },
-    { name: 'Marta Baudracco', hobby: 'Paddle', gender: 'F' },
-    { name: 'Paolo Giordano', hobby: 'Palestra', gender: 'M' },
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M', isPro: false },
+    { name: 'Giada Valinotto', hobby: 'Paddle', gender: 'F', isPro: true },
+    { name: 'Luca Dellavalle', hobby: 'Poligono', gender: 'M', isPro: true },
+    { name: 'Bianca Teleman', hobby: 'Karate', gender: 'F', isPro: true },
+    { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Samuele De Toffol', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Luna Martino', hobby: 'Poligono', gender: 'F', isPro: true },
+    { name: 'Matteo Cela', hobby: 'Karate', gender: 'M', isPro: true },
+    { name: 'Gabriele Leone', hobby: 'Palestra', gender: 'M', isPro: false },
+    { name: 'Martina Velardi', hobby: 'Karate', gender: 'F', isPro: false },
+    { name: 'Marta Baudracco', hobby: 'Paddle', gender: 'F', isPro: false },
+    { name: 'Paolo Giordano', hobby: 'Palestra', gender: 'M', isPro: true },
   ]
 
   student: any = {};
